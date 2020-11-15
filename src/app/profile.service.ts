02a31747acf6ec453fca4ac3a0f884193f16d601
Preston-Too <toopreston@gmail.com>
2020-11-15 09:36:38 +0300

    add a function to display profile info and repos

---
+++ src/app/profile.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProfileService {
+
+  constructor(private http:HttpClient) { }
+
+  getProfileInformation(user){
+    return this.http.get('https://api.github.com/users/'+user+ '?access_token='+environment.apiKey)
+  }
+  getProfileRepos(user){
+    return this.http.get('https://api.github.com/users/'+user+'/repos'+'?access_token='+environment.apiKey)
+  }
+  
+}
