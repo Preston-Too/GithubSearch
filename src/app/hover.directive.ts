973c078f6ec4931decd0bb8681d7c914cbc9863c
Preston-Too <toopreston@gmail.com>
2020-11-16 06:58:58 +0300

    add on hover event listener

---
+++ src/app/hover.directive.ts
@@ -0,0 +1,27 @@
+import { Directive, ElementRef, HostListener, Renderer2 } from '@angular/core';
+
+@Directive({
+  selector: '[appHover]'
+})
+export class HoverDirective {
+
+  constructor(private elem: ElementRef, private renderer: Renderer2) { }
+  setColor(color: string) {
+    this.renderer.setStyle(
+      this.elem.nativeElement, 'backgroundColor', color
+    );
+  }
+
+  @HostListener('mouseover')
+  onMouseOver() {
+    this.setColor('cyan')
+  }
+
+  @HostListener('mouseout')
+  onMouseOut() {
+    this.setColor('#fff')
+  }
+
+}
+
+
